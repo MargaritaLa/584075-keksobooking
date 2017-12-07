9426b182bd7102037592e2f1e8d2e1c9cd7000b2
Маргарита Лаевская <terra_82@mail.ru>
2017-12-07 03:05:25 +0300

    задание module4-task1 до eslint

--- js/map.js
+++ js/map.js
@@ -2,6 +2,9 @@
 
 (function () {
 
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
   var imageStartRange = 1;
   var imageEndRange = 8;
   var cardObjectsCount = 8;
@@ -14,28 +17,48 @@
   var objectPhotos = [];
   var cardObjectsArray = [];
 
+
   /*  блок куда будем отрисовывать наши pin */
   var containerForPin = document.querySelector('.map__pins');
   /*  рабочий блок */
   var blockMap = document.querySelector('.map');
 
   /* блок формы */
-  var blockForm =  document.querySelector('.map__filters-container');
+  var blockForm =  document.querySelector('.notice__form');
   blockForm.classList.add('notice__form--disabled');
 
   /* дизаблим все поля формы */
-  doDisabledFieldsForm();
+  changeStateFieldsForm(true);
 
-  var mapMainPin = document.querySelector('.map__pin--main');
+  var mapMainPin = document.querySelector('.map__pin.map__pin--main');
   mapMainPin.addEventListener('mouseup', activateFormAndMap);
 
   createObjectsArray(cardObjectsCount);
 
   /* выводим все объекты перед блоком .map__filters-container */
-  renderObjects(blockMap, cardObjectsArray);
+
 
   /* выводим все пинв в  блок .map__pins */
   renderPins(containerForPin, cardObjectsArray);
+
+  /* обрабатываем нажатие по пину */
+  var mapPins = document.querySelectorAll('.map__pin');
+  mapPins.forEach(function(pin){
+    pin.addEventListener('mouseup', function(event) {
+      openPopupCardObject(this);
+    });
+
+    pin.addEventListener('keydown', function(event) {
+      if (event.keyCode === ENTER_KEYCODE) {
+       openPopupCardObject(this);
+     }
+   });
+  });
+
+  document.addEventListener('keydown', onPopupEscPress);
+
+  /*  коллекция карточек объявлений*/
+  var matchesCardObjects = document.querySelectorAll('.map__card ');
 
   /* создаем массив объектов "Объявление" */
   function createObjectsArray(cardsCount) {
@@ -80,30 +103,40 @@
   }
 
   /*  выводим все объекты перед блоком .map__filters-container */
-  function renderObjects(benchmark, cardObjectsResultingArray) {
+  function renderFirstObject(parentContainer, cardObjectsResultingArray, number) {
 
     /* ссылка на пустой объект */
-    var objectsFragment = document.createDocumentFragment();
+    var objectFragment = document.createDocumentFragment();
     /* шаблон в который копируем объект DocumentFragment (явл-ся NODом) */
     var mapPopupObjectTemplate = document.querySelector('template').content.querySelector('article.map__card');
 
     /* блок перед которым будем вставлять блоки с нашими элементами */
     var lookoutEl = document.querySelector('.map__filters-container');
-    for (var i = 0; i <= cardObjectsArray.length - 1; i++) {
-      objectsFragment.appendChild(renderObject(mapPopupObjectTemplate, cardObjectsResultingArray[i]));
-    }
+    objectFragment.appendChild(renderObject(mapPopupObjectTemplate, cardObjectsResultingArray[number]));
 
-    benchmark.insertBefore(objectsFragment, lookoutEl);
+    parentContainer.insertBefore(objectFragment, lookoutEl);
   }
 
   /*  функция вывода объекта в верстку / отрисовка шаблона объекта в документ */
   function renderObject(mapPopupObjectTemplate, object) {
 
     var pinObjectNode = mapPopupObjectTemplate.cloneNode(true);
+    var popupClose = pinObjectNode.querySelector('.popup__close');
     pinObjectNode.querySelector('.popup__avatar').src = object.author.avatar;
     pinObjectNode.querySelector('h3').textContent = object.offer.title;
     pinObjectNode.querySelector('p small').textContent = object.offer.address;
     pinObjectNode.querySelector('.popup__price').innerHTML = object.offer.price + ' &#x20bd;/ночь';
+    popupClose.setAttribute('tabindex','0');
+
+    popupClose.addEventListener('click', function () {
+      closePopup();
+    });
+
+    popupClose.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        closePopup();
+      }
+    });
 
     if (object.offer.type === 'flat') {
       object.offer.type = 'Квартира';
@@ -115,7 +148,6 @@
       object.offer.type = 'Дом';
     }
     pinObjectNode.querySelector('h4').textContent = object.offer.type;
-
     pinObjectNode.querySelector('.capacity').textContent = object.offer.rooms + ' комнаты для ' + object.offer.guests + ' гостей';
     pinObjectNode.querySelector('.stay__time').textContent = 'Заезд после ' + object.offer.checkin + ', выезд до ' + object.offer.checkout;
     pinObjectNode.querySelector('.description').textContent = object.offer.description;
@@ -145,79 +177,128 @@
     var mapPinsObjectTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
     for (var i = 0; i <= objectsArray.length - 1; i++) {
-      objectsFragment.appendChild(renderPin(mapPinsObjectTemplate, objectsArray[i]));
-    }
-
-    container.appendChild(objectsFragment);
-  }
-
-  /*  функция вывода пина в верстку / отрисовка шаблона объекта в документ */
-  function renderPin(mapPinsObjectTemplate, pin) {
-
-    var pinNode = mapPinsObjectTemplate.cloneNode(true);
-
-    var marginLeft = pinNode.querySelector('img').width / 2;
-    var marginTop = pinNode.querySelector('img').height / 2;
-
-    pinNode.querySelector('img').src = pin.author.avatar;
-    pinNode.style.left = (pin.location.x - marginLeft) + 'px';
-    pinNode.style.top = (pin.location.y - marginTop) + 'px';
-    return pinNode;
-
-  }
-
-  /*  функция перемешивания значений в массиве */
-  function shuffleArray(array) {
-    return array.sort(function () {
-      return Math.random() - 0.5;
-    });
-  }
-
-  /*  получить случайное число в диапозоне от min до max включительно */
-  function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  /*  получить массив случайных и неповторяющихся чисел */
-  function getRandomNonRepeatingValue(firstNumber, lastNumber) {
-
-    var nonRepeatingValuesArray = [];
-
-    for (var i = firstNumber; i <= lastNumber; i++) {
-      nonRepeatingValuesArray.push(i);
-    }
-
-    return shuffleArray(nonRepeatingValuesArray);
-
-  }
-
-  /* получение массива что есть в объекте wifi, parking и пр. */
-  function getObjectFeatures(allFeatures) {
-
-    var objectFeatures = [];
-
-    allFeatures = shuffleArray(allFeatures);
-
-    for (var i = 0; i < getRandomValue(0, allFeatures.length); i++) {
-      objectFeatures.push(allFeatures[i]);
-    }
-
-    return objectFeatures;
-
-  }
-
-  function doDisabledFieldsForm() {
-
-    var matches = document.querySelectorAll('.map__filters select, .map__filters input');
-
-    for (var i = 0; i < matches.length; i++) {
-     matches[i].disabled = true;
+     var pinNode = renderPin(mapPinsObjectTemplate, objectsArray[i]);
+     pinNode.setAttribute('data-objectId', i);
+     objectsFragment.appendChild(pinNode);
    }
 
+   container.appendChild(objectsFragment);
  }
 
+ /*  функция вывода пина в верстку / отрисовка шаблона объекта в документ */
+ function renderPin(mapPinsObjectTemplate, pin) {
+
+  var pinNode = mapPinsObjectTemplate.cloneNode(true);
+
+  var marginLeft = pinNode.querySelector('img').width / 2;
+  var marginTop = pinNode.querySelector('img').height / 2;
+
+  pinNode.querySelector('img').src = pin.author.avatar;
+  pinNode.style.left = (pin.location.x - marginLeft) + 'px';
+  pinNode.style.top = (pin.location.y - marginTop) + 'px';
+  pinNode.setAttribute('tabindex','0');
+  return pinNode;
+
+}
+
+/*  функция перемешивания значений в массиве */
+function shuffleArray(array) {
+  return array.sort(function () {
+    return Math.random() - 0.5;
+  });
+}
+
+/*  получить случайное число в диапозоне от min до max включительно */
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/*  получить массив случайных и неповторяющихся чисел */
+function getRandomNonRepeatingValue(firstNumber, lastNumber) {
+
+  var nonRepeatingValuesArray = [];
+
+  for (var i = firstNumber; i <= lastNumber; i++) {
+    nonRepeatingValuesArray.push(i);
+  }
+
+  return shuffleArray(nonRepeatingValuesArray);
+
+}
+
+/* получение массива что есть в объекте wifi, parking и пр. */
+function getObjectFeatures(allFeatures) {
+
+  var objectFeatures = [];
+
+  allFeatures = shuffleArray(allFeatures);
+
+  for (var i = 0; i < getRandomValue(0, allFeatures.length); i++) {
+    objectFeatures.push(allFeatures[i]);
+  }
+
+  return objectFeatures;
+
+}
+
+ // функции обработки событий
+
  function activateFormAndMap(){
+
   blockMap.classList.remove('map--faded');
+  blockForm.classList.remove('notice__form--disabled');
+  changeStateFieldsForm(false);
+
+}
+
+function deleteCardObject() {
+  var destroyed = document.querySelector('.map__card');
+  if(destroyed){
+    destroyed.remove();
+  }
+}
+
+function changeStateFieldsForm(flag) {
+  var matches = document.querySelectorAll('.map__filters select, .map__filters input');
+  for (var i = 0; i < matches.length; i++) {
+   matches[i].disabled = flag;
+ }
+}
+
+function deleteActiveClass(){
+  for (var i = 0; i <= mapPins.length - 1; i++) {
+    mapPins[i].classList.remove('map__pin--active');
+  }
+}
+
+/* закрытие окна */
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+function closePopup() {
+  var focused = document.activeElement;
+  deleteCardObject();
+  deleteActiveClass();
+};
+
+/* открытие окна */
+
+function openPopup() {
+  var focused = document.activeElement;
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+function openPopupCardObject(pin) {
+ if (!pin.classList.contains('map__pin--main')){
+  deleteActiveClass();
+  pin.classList.add('map__pin--active');
+  deleteCardObject();
+  var indexObject = pin.getAttribute('data-objectid');
+  renderFirstObject(blockMap, cardObjectsArray, indexObject);
+}
 }
 
 })();
